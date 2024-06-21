0e9976668062a3f1d3b10aab9406e34dbbc4d05a
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-06-21 13:50:26 +0300

    test

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import express from "express";
 import "dotenv/config";
 import logger from "morgan";
 import cors from "cors";
+import { readFile } from "fs/promises";
 //Path
 import authRouter from "./routes/auth-routers.js";
 import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./swagger.js";
+const swaggerSpec = JSON.parse(await readFile("./swagger.json"));
 
 const app = express();
 
@@ -14,8 +15,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use("/users", authRouter);
+app.use("/api/users", authRouter);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found path" });
--- routes/auth-routers.js
+++ routes/auth-routers.js
@@ -18,26 +18,3 @@ authRouter.post("/logout", authenticate, authControler.signout);
 authRouter.get("/test", authControler.test);
 
 export default authRouter;
-
-/**
- * @swagger
- * /users:
- *   get:
- *     summary: Retrieve a list of users
- *     responses:
- *       200:
- *         description: A list of users
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     example: 1
- *                   name:
- *                     type: string
- *                     example: John Doe
- */
