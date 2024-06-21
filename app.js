1490ec7a397f9964e9533d6a3a446a09aca46d65
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-06-21 15:44:00 +0300

    test seting swwager

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use("/api/users", authRouter);
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/users", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found path" });
