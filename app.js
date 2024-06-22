454fb0b9dd6e102d4af82a29f1897b982ab738e5
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-06-22 10:06:17 +0300

    add new path

--- app.js
+++ app.js
@@ -6,34 +6,37 @@ import { readFile } from "fs/promises";
 import { fileURLToPath } from "url";
 import path from "path";
 import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 
 //Path
 import authRouter from "./routes/auth-routers.js";
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
+// Swagger
+const swaggerDocument = JSON.parse(await readFile("./swagger.json"));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const swaggerPath = path.join(__dirname, "swagger.json");
+
+// let dataJson;
+
+// async function loadSwagger() {
+//   try {
+//     const swaggerDocument = JSON.parse(await readFile(swaggerPath, "utf8"));
+//     dataJson = swaggerDocument;
+//   } catch (error) {
+//     console.error("Error reading swagger.json:", error);
+//     process.exit(1); // Завершуємо процес з помилкою
+//   }
+// }
+// loadSwagger();
+console.log(swaggerDocument);
 const options = {
-  customCssUrl: CSS_URL,
+  swaggerDefinition: swaggerDocument,
   apis: [],
 };
-// Swagger
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const swaggerPath = path.join(__dirname, "swagger.json");
 
-let swaggerSpec;
-
-async function loadSwagger() {
-  try {
-    const swaggerDocument = JSON.parse(await readFile(swaggerPath, "utf8"));
-    swaggerSpec = swaggerDocument;
-  } catch (error) {
-    console.error("Error reading swagger.json:", error);
-    process.exit(1); // Завершуємо процес з помилкою
-  }
-}
-
-await loadSwagger(); // Завантажуємо swaggerSpec перед налаштуванням маршруту
+const swaggerSpec = swaggerJSDoc(options);
+// await loadSwagger(); // Завантажуємо swaggerSpec перед налаштуванням маршруту
 
 const app = express();
 
@@ -41,7 +44,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/users", authRouter);
 
 app.use((req, res) => {
