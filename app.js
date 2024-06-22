2b74b70d94f76213b8fa54ad3418218335d94db2
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-06-22 13:35:24 +0300

    change json exemple

--- app.js
+++ app.js
@@ -16,13 +16,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const swaggerPath = path.join(__dirname, "swagger.json");
 const swaggerDocument = JSON.parse(await readFile(swaggerPath, "utf8"));
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const options = {
   swaggerDefinition: swaggerDocument,
   apis: [],
+  customCssUrl: CSS_URL,
 };
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const swaggerSpec = swaggerJSDoc(options);
 
@@ -32,11 +33,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(
-  "/api/docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL })
-);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/users", authRouter);
 
 app.use((req, res) => {
